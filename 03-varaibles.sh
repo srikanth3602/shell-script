7f2c864e36a2d58c89b96a441c220d9f8e7ac355
srikanth3602 <srikanthlakshman9999@gmail.com>
2026-02-15 15:25:57 +0530

    convo

---
+++ 03-varaibles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=Suresh
+person2=ramesh
+
+echo "$person1 : Hi $person2"
+echo "$person2 : Hello $person1"
+echo "$person1 : how are you donig $person2"
+echo "$person2 : Iam doing great "
